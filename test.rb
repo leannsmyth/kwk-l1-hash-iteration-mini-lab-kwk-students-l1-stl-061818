a28540b07da64c43a01d51c6aaeec383766c9a74
leannsmyth <leannmsmyth@gmail.com>
2018-06-22 18:20:45 +0000

    automatically backed up by learn

--- test.rb
+++ test.rb
@@ -44,13 +44,13 @@ class Quizzer
   
   Quizzer.add_song_lyric(Hsm.new("It's time to show how to be a superhero!", "the boys are back"))
   Quizzer.add_song_lyric(Hsm.new("We make each other strong", "were all in this together"))
-  Quizzer.add_song_lyric(Hsm.new("No more waking up at 6 am", "what time is it?"))
+  Quizzer.add_song_lyric(Hsm.new("No more waking up at 6 am", "what time is it"))
   Quizzer.add_song_lyric(Hsm.new("Hey batter batter, hey batter batter swing!", "i dont dance"))
   Quizzer.add_song_lyric(Hsm.new("Stick to the stuff you know", "stick to the status quo'"))
   
   Quizzer.quiz_song_lyric(Hsm.new("It's time to show how to be a superhero!", "the boys are back"))
   Quizzer.quiz_song_lyric(Hsm.new("We make each other strong", "were all in this together"))
-  Quizzer.quiz_song_lyric(Hsm.new("No more waking up at 6 am", "what time is it?"))
+  Quizzer.quiz_song_lyric(Hsm.new("No more waking up at 6 am", "what time is it"))
   Quizzer.quiz_song_lyric(Hsm.new("Hey batter batter, hey batter batter swing!", "i dont dance"))
   Quizzer.quiz_song_lyric(Hsm.new("Stick to the stuff you know", "stick to the status quo"))
   
