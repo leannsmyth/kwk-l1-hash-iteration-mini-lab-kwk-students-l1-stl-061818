d5984fe8bea9f9adf6eb11ef20324b2089735972
leannsmyth <leannmsmyth@gmail.com>
2018-06-22 18:17:24 +0000

    automatically backed up by learn

---
+++ hash_lab.rb
@@ -0,0 +1,36 @@
+# def places
+  dream_vacation = {
+    "Hanna" => "bora bora",
+    "Heidi" =>"Fiji",
+    "Maggie" => "London",
+    "Chole" => "San Francisco",
+    "Carmen" => "Paris"
+  }
+#   puts dream_vacation
+#   puts dream_vacation.keys
+#   puts dream_vacation.values
+# end
+# places
+ 
+
+dream_vacation.each do |classmates, vacs|
+  puts "#{classmates}, wants to go to #{vacs}"
+  puts ""
+end
+
+
+
+
+
+
+# names = ["Hanna", "Heidi", "Maggie", "Chloe", "Carmen"]
+# spots = ["bora bora", "fiji", "london", "San Francisco", "Paris"]
+
+# favorite_place = {}
+# index = 0
+# names.each do |girls|
+#   favorite_place[girls] = spots[index]
+#   index += 1
+# end
+# puts favorite_place
+ 
